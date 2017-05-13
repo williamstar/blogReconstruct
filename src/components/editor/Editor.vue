2b28b0946dfab01894cdb0737557d9eb09be4f30
william <740056233@qq.com>
2017-05-13 12:17:10 +0800

    封面上传

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -4,42 +4,51 @@
       <el-col :span="4">left-part</el-col>
       <el-col :span="16">
         <el-form ref="form" label-width="40px">
-          <!--标题和时间-->
-          <el-row type="flex" justify="space-between">
-            <el-col :span="10">
-              <el-form-item label="标题">
-                <el-input v-model="form.title"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="9" :offset="5">
-              <el-form-item label="时间">
-                <el-input disabled></el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <!--分类和标签和自动保存-->
           <el-row>
-            <el-col :span="5">
-              <el-form-item label="分类">
-                <el-select v-model="form.category">
-                  <el-option v-for="option in options" :value="option" :key="option" :label="option"> </el-option>
-                </el-select>
-              </el-form-item>
+            <el-col :span="20">
+
+              <!--标题和时间-->
+              <el-row type="flex" justify="space-between">
+                <el-col :span="12">
+                  <el-form-item label="标题">
+                    <el-input v-model="form.title"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="9" :offset="3">
+                  <el-form-item label="分类">
+                    <el-select v-model="form.category">
+                      <el-option v-for="option in options" :value="option" :key="option" :label="option"> </el-option>
+                    </el-select>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <!--分类和标签和自动保存-->
+              <el-row>
+                <el-col :span="9">
+                  <el-form-item label="标签">
+                    <el-tag v-for="tag in tags" :key="tag" :closable="true" @close="removeTag(tag)" class="tag" type="primary" color="#fff">
+                      {{tag}}
+                    </el-tag>
+                    <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+                    </el-input>
+                    <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="4">
+                  <el-form-item label="自动保存" label-width="80px">
+                    <el-switch v-model="autoSave"></el-switch>
+                  </el-form-item>
+                </el-col>
+              </el-row>
             </el-col>
-            <el-col :span="10">
-              <el-form-item label="标签">
-                <el-tag v-for="tag in tags" :key="tag" :closable="true" @close="removeTag(tag)" type="primary" color="#fff">
-                  {{tag}}
-                </el-tag>
-                <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
-                </el-input>
-                <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
-              </el-form-item>
-            </el-col>
+
             <el-col :span="4">
-              <el-form-item label="自动保存" label-width="80px">
-                <el-switch v-model="autoSave"></el-switch>
-              </el-form-item>
+              <!--封面-->
+              <el-button v-if="!haveCover" class="btn-cover" @click="selectCover">
+                <i class="el-icon-plus icon-plus"></i>
+              </el-button>
+              <img v-else class="upload-cover" alt="封面" @click="selectCover">
+              <transparent-file-elm :selector="'upload-cover'" :need-hash="true" @had:cover="setCover" ref="uploadCover"></transparent-file-elm>
             </el-col>
           </el-row>
           <!--博客框-->
@@ -59,6 +68,7 @@
 import $ from 'jquery';
 import editormd from 'editormd';
 /* eslint-enable */
+import transparentFileElm from '@/components/smallcomponents/TransparentFileElm';
 
 export default {
   data() {
@@ -68,6 +78,7 @@ export default {
       inputVisible: false,
       inputValue: '',
       autoSave: true,
+      haveCover: false,
       form: {
         title: '',
         category: '',
@@ -98,6 +109,15 @@ export default {
     });
   },
   methods: {
+    setCover(dataUrl) {
+      this.haveCover = true;
+      this.$nextTick(() => {
+        document.querySelector('.upload-cover').src = dataUrl;
+      });
+    },
+    selectCover() {
+      this.$refs.uploadCover.open();
+    },
     removeTag(tag) {
       let index = this.tags.indexOf(tag);
       this.tags.splice(index, 1);
@@ -111,11 +131,21 @@ export default {
     handleInputConfirm() {
       let inputValue = this.inputValue;
       if (inputValue) {
-        this.tags.push(inputValue);
+        if (this.tags.length <= 4) {
+          this.tags.push(inputValue);
+        } else {
+          this.$message({
+            message: '不能超过5个标签',
+            type: 'warning',
+          });
+        }
       }
       this.inputVisible = false;
       this.inputValue = '';
     },
+  },
+  components: {
+    transparentFileElm,
   },
 };
 </script>
@@ -125,10 +155,28 @@ export default {
   label {
     color: #fff;
   }
+  tag {
+    margin-right: 5px;
+  }
+  .btn-cover {
+    width: 125px;
+    height: 125px;
+    .avatar-upload {
+      font-size: 26px;
+      color: #48d0d3;
+    }
+  }
+  .upload-cover {
+    width: 125px;
+    height: 125px;
+    object-fit: cover;
+    cursor: pointer;
+  }
   .input-new-tag {
     width: 77.67px;
   }
   .editor-wrapper {
+    margin-top: 10px;
     height: 800px;
   }
 }
---
+++ src/components/smallcomponents/TransparentFileElm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <input type="file" @change="replaceImg($event)" class="transparent-file-elm" :class="'hidden-'+ selector">
+  </div>
+</template>
+
+<script type="text/javascript">
+import SparkMD5 from '../../../static/js/spark-md5.min';
+
+export default {
+  props: {
+    selector: String,
+    needHash: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+  },
+  data() {
+    return {
+      node: null,
+    };
+  },
+  methods: {
+    open() {
+      if (!this.node) {
+        this.node = document.querySelector(`.transparent-file-elm.hidden-${this.selector}`);
+      }
+      this.node.click();
+    },
+    replaceImg(event) {
+      let file = event.target.files[0];
+      let fr = new FileReader();
+      let self = this;
+      fr.onload = () => {
+        if (self.needHash) {
+          let spark = new SparkMD5();
+          spark.append(fr.result);
+          self.$emit('had:cover', fr.result, spark.end());
+        } else {
+          self.$emit('had:cover', fr.result);
+        }
+      };
+      fr.readAsDataURL(file);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.transparent-file-elm {
+  display: none;
+}
+</style>
