857bebfbe85349dc55519821d6e2915d5cfd4925
william <740056233@qq.com>
2017-05-23 09:33:49 +0800

    初始化个人设置编辑

---
+++ src/components/config/Config.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='config-module'>
+    <div class="category-manager">
+      分类管理
+    </div>
+    <div class="passwd-modify">
+      修改密码
+    </div>
+    <div class="draft-manager">
+      草稿管理
+    </div>
+    <div class="cover-manager">
+      <span class="text">用户头像替换</span>
+      <img src="/api/image/coverImg" alt="用户头像" class="cover">
+    </div>
+    <div class="ban-ip">
+      屏蔽IP
+    </div>
+    <div class="project-upload">
+      上传项目
+    </div>
+    <div class="link-manager">
+      链接上传
+    </div>
+    <div class="self-intro-edit">
+      编辑个人简介
+    </div>
+  </div>
+</template>
+<script type='text/javascript'>
+export default{
+
+};
+</script>
+<style lang='scss' scoped>
+.config-module {
+  position: relative;
+  margin: 0 auto;
+  width: 1200px;
+  height: 800px;
+  color: #fff;
+}
+</style>
