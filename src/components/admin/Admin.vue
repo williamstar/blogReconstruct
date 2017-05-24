fd23366ef4ae9a81f3cadd556d3f995045a9d1fb
william <740056233@qq.com>
2017-05-24 15:51:08 +0800

    增加搜索状态

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='admin-module'>
     <div class="platform">
-      <div class="button-groups">
+      <div class="nav-bar">
         <router-link to="/edit">
           <el-button icon="plus" type="primary">添加新博客</el-button>
         </router-link>
@@ -18,26 +18,31 @@
           </el-button>
         </router-link>
       </div>
+      <el-form :inline="true" label-width="50px" class="filter">
+        <el-form-item label="博客状态">
+          <el-select v-model="isDraft" @change="getBlog">
+            <el-option v-for="(status, index) in blogStatuss" :value="index" :label="status" :key="index"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="分类">
+          <el-select v-model="categoryIndex" @change="getBlog">
+            <el-option v-for="category in categories" :value="category.id" :label="category.val" :key="category.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="排序">
+          <el-select v-model="sortedIndex" @change="getBlog">
+            <el-option v-for="(status, index) in sortedStatuss" :value="index" :label="status.text" :key="status.val"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="queryStr" placeholder="全局搜索" icon="search" :on-icon-click="handlerSearch"></el-input>
+        </el-form-item>
+      </el-form>
     </div>
     <blog-iterator :blogs="blogs" :admin="true"></blog-iterator>
     <div class="pagination">
-      <span @click="jump(1)" class="changepage f-l-page">
-        <i class="el-icon-d-arrow-left"></i>
-      </span>
-      <span @click="prePage" class="changepage" :class="{'forbid': currentPage === 1}">
-        <i class="el-icon-arrow-left"></i>
-      </span>
-      <span v-for="i in pageSize" class="page-btn" :class="{'light': i === currentPage}" @click="jump(i)">
-        <span class="page-text">
-          {{i}}
-        </span>
-      </span>
-      <span @click="nextPage" class="changepage" :class="{'forbid': currentPage === pageSize}">
-        <i class="el-icon-arrow-right"></i>
-      </span>
-      <span @click="jump(pageSize)" class="changepage f-l-page">
-        <i class="el-icon-d-arrow-right"></i>
-      </span>
+      <el-pagination layout="prev, pager, next" :total="total" :current-page.sync="currentPage" @current-change="handleCurrentChange">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -49,7 +54,40 @@ const OK = 'success';
 export default {
   data() {
     return {
+      blogStatuss: ['完成', '草稿'],
+      sortedStatuss: [
+        {
+          val: 'desc',
+          key: 'createDate',
+          text: '最近',
+        },
+        {
+          val: 'asc',
+          key: 'createDate',
+          text: '最久',
+        },
+        {
+          val: 'desc',
+          key: 'exploredTime',
+          text: '最多浏览量',
+        },
+        {
+          val: 'desc',
+          key: 'commentNum',
+          text: '最多评论量',
+        },
+      ],
+      categories: [
+        {
+          id: -1,
+          val: '全部',
+        },
+      ],
+      categoryIndex: -1,
+      sortedIndex: 0,
+      isDraft: 0,
       currentPage: 1,
+      queryStr: '',
       blogs: [],
       total: 0,
       limit: 10,
@@ -70,11 +108,12 @@ export default {
   created() {
     this
       .$http
-      .get('/api/count-blog')
+      .get('/api/preload-data')
       .then((res) => {
         res = res.body;
         if (res.status === OK) {
-          this.total = res.data;
+          this.total = res.data.total;
+          this.categories = this.categories.concat(res.data.categories);
         }
       });
   },
@@ -99,9 +138,15 @@ export default {
           }
         });
     },
+    handleCurrentChange() {
+      this.getBlog();
+    },
+    handlerSearch() {
+
+    },
     getBlog() {
       let defaultUrl = '/api/admin';
-      defaultUrl = `/api/admin?page=${this.currentPage}`;
+      defaultUrl = `/api/admin?page=${this.currentPage}&isDraft=${this.isDraft}&categoryId=${this.categoryIndex}&sortedKey=${this.sortedStatuss[this.sortedIndex].key}&sortedVal=${this.sortedStatuss[this.sortedIndex].val}&queryStr=${this.queryStr}`;
       this
         .$http
         .get(defaultUrl)
@@ -112,19 +157,6 @@ export default {
           }
         });
     },
-    prePage() {
-      if (this.currentPage > 1) {
-        this.currentPage -= 1;
-      }
-    },
-    nextPage() {
-      if (this.currentPage < this.pageSize) {
-        this.currentPage += 1;
-      }
-    },
-    jump(page) {
-      this.currentPage = page;
-    },
   },
   components: {
     blogIterator,
@@ -134,36 +166,22 @@ export default {
 <style lang='scss'>
 .admin-module {
   padding: 10px;
+  label {
+    color: #fff;
+  }
+  .platform {
+    padding: 10px 0 10px 20px;
+    .el-button {
+      margin-right: 20px;
+    }
+    .filter {
+      margin-top: 20px;
+    }
+  }
   .pagination {
-    position: relative;
+    margin-top: 25px;
     display: flex;
     justify-content: center;
-    align-items: center;
-    margin: 50px 0;
-    color: #fff;
-    font-size: 17px;
-    .f-l-page {
-      margin: 0 8px;
-    }
-    .page-btn {
-      margin: 0 15px;
-      display: inline-block;
-      padding: 2px 2px;
-      background: #fff;
-      cursor: pointer;
-      &.light {
-        background: #20a0ff;
-        .page-text {
-          color: #fff;
-        }
-      }
-    }
-    .changepage {
-      cursor: pointer;
-      &.forbid {
-        cursor: not-allowed;
-      }
-    }
   }
 }
 </style>
